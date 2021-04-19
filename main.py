3f0b547f2b3133ce75f8a80c5ab6e0ba4903f324
jroux <jroux@chapman.edu>
2021-04-19 12:10:51 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#Jessica Roux
+#Student ID: 2317255
+#Chapman Email: jroux@chapman.edu
+#Course: 408-01
+#Assignment 4
+
+import scrA4 as scr
+
+# Execute and run program/functions
+def runAssignment4():
+    scr.genData()
+    scr.importData()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    runAssignment4()
+

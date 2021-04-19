c6c5eec9de5a812306d942c8c4cbdd9fe78c2ecd
jroux <jroux@chapman.edu>
2021-04-19 09:49:50 -0700

    initial commit

---
+++ soccerMain.py
@@ -0,0 +1,18 @@
+#Jessica Roux
+#Student ID: 2317255
+#Chapman Email: jroux@chapman.edu
+#Course: 408-01
+#Assignment 4
+
+import soccer as soc
+
+# Execute and run program/functions
+def runAssignment4():
+    soc.genData()
+    soc.importData()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    runAssignment4()
+
